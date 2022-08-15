e8306fae8fc781c95eb2018c4f5f40e88b32825b
Bryan Hu <bryan.hu.2020@gmail.com>
2022-08-15 12:00:33 -0700

    :bug: Oops- forgot to remove test data

--- src/lib/InfoInput.svelte
+++ src/lib/InfoInput.svelte
@@ -4,62 +4,13 @@
 	import type { Schedule } from './InfoInput.d';
 	let name: string = '';
 	let periods: Schedule = {
-		'1a': {
-			id: '8b2098d8-59d2-406d-8ada-55fc994550ce',
-			name: 'bible 8',
-			teacher_first: 'john',
-			teacher_last: 'delke',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'2a': {
-			id: 'e44b1ee7-9346-4beb-a08a-f65280b94555',
-			name: 'band 2',
-			teacher_first: 'shane',
-			teacher_last: 'ryan',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'3a': {
-			id: 'ca70c336-f30c-4b9f-a79a-43b8e3b02862',
-			name: 'pltw space and electricity',
-			teacher_first: 'dwain',
-			teacher_last: 'fairweather',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'4a': {
-			id: '70a928a3-b7e0-4583-b1af-c8af4bbe9126',
-			name: 'physical science',
-			teacher_first: 'michael',
-			teacher_last: 'pond',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'1b': {
-			id: '08b6e7b7-0ffc-4d11-86a7-b37d4700386d',
-			name: 'boys pe 7/8',
-			teacher_first: 'shea',
-			teacher_last: 'coleman',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'2b': {
-			id: '45098bed-503e-4c99-8158-bd90212d2653',
-			name: 'history 8',
-			teacher_first: 'dwain',
-			teacher_last: 'fairweather',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
-		'3b': {
-			id: '279966c2-44c3-4f85-9892-83b8e355f11a',
-			name: 'algebra 2 honors',
-			teacher_first: 'dan',
-			teacher_last: 'yuen',
-			room: '99df5949-3947-4342-a5b0-d8260925528d'
-		},
-
+		'1a': null,
+		'2a': null,
+		'3a': null,
+		'4a': null,
+		'1b': null,
+		'2b': null,
+		'3b': null,
 		'4b': null
 	};
 	const dispatch = createEventDispatcher();
