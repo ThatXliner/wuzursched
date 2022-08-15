8aa09a97b62b58df74fbbd190a6aa7bdbfe258fb
Bryan Hu <bryan.hu.2020@gmail.com>
2022-08-15 13:32:23 -0700

    :wrench: Changed environment variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
