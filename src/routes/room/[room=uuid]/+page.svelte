8fd3ba9cb0f931e055f92da3ed7e4798e0719342
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-03 17:35:04 -0700

    :lipstick: Shrank clipboard button

--- src/routes/room/[room=uuid]/+page.svelte
+++ src/routes/room/[room=uuid]/+page.svelte
@@ -178,29 +178,31 @@
 				>{$page.params['room'].slice(0, 8)}</code
 			>
 		</h1>
-		<div class="flex justify-evenly flex-row space-x-4">
-			<button
-				class="btn btn-accent"
-				on:click={() => {
-					navigator.clipboard.writeText(window.location.href).then(() => {
-						addToast('Link copied to clipboard', 'success');
-					});
-				}}
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M10.5 3A1.501 1.501 0 009 4.5h6A1.5 1.5 0 0013.5 3h-3zm-2.693.178A3 3 0 0110.5 1.5h3a3 3 0 012.694 1.678c.497.042.992.092 1.486.15 1.497.173 2.57 1.46 2.57 2.929V19.5a3 3 0 01-3 3H6.75a3 3 0 01-3-3V6.257c0-1.47 1.073-2.756 2.57-2.93.493-.057.989-.107 1.487-.15z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				Share Room Link
-			</button>
-			<a href="/" class="btn"
+		<div class="flex justify-evenly flex-row space-x-4 mt-3">
+			<!-- <div class="flex justify-evenly flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0"> -->
+			<div class="tooltip tooltip-right md:tooltip-top" data-tip="Copy room link to clipboard">
+				<button
+					class="btn btn-accent"
+					on:click={() => {
+						navigator.clipboard.writeText(window.location.href).then(() => {
+							addToast('Room URL copied to clipboard', 'success');
+						});
+					}}
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M10.5 3A1.501 1.501 0 009 4.5h6A1.5 1.5 0 0013.5 3h-3zm-2.693.178A3 3 0 0110.5 1.5h3a3 3 0 012.694 1.678c.497.042.992.092 1.486.15 1.497.173 2.57 1.46 2.57 2.929V19.5a3 3 0 01-3 3H6.75a3 3 0 01-3-3V6.257c0-1.47 1.073-2.756 2.57-2.93.493-.057.989-.107 1.487-.15z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
+			</div>
+			<a href="/" class="btn" title="Go home"
 				><svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6"
