2c06b3b1ca2e21214ecd69bb24b54eaed9dbecc6
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-19 16:27:18 -0700

    Work now

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import dotenv from 'dotenv';
 
-dotenv.config();
-console.log(process.env.VITE_SUPABASE_URL);
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm run build && pnpm run preview',
