868e031b2cdd8793f84c91c56833b8d8e0753382
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-19 16:06:51 -0700

    Debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+console.log(process.env.VITE_SUPABASE_URL);
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm run build && pnpm run preview',
