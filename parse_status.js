2e894bd3d05b7033602ac9e1636a09b1ca923c4b
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-19 08:37:06 -0700

    Should be more reliable now

--- parse_status.js
+++ parse_status.js
@@ -7,8 +7,19 @@
 //       JWT secret: super-secret-jwt-token-with-at-least-32-characters-long
 //         anon key: <key>
 // service_role key: <key>
-import fs from 'fs';
-const stdin = fs.readFileSync(process.stdin.fd, 'utf-8');
+import { readFile } from 'fs';
+const readFilePromise = (...args) =>
+	new Promise((resolve, reject) => {
+		readFile(...args, (error, result) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(result);
+			}
+		});
+	});
+
+const stdin = await readFilePromise(process.stdin.fd, 'utf-8');
 const REMAP = {
 	'API URL': 'VITE_SUPABASE_URL',
 	'DB URL': 'DB_URL',
