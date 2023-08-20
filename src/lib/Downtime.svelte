506be2dfee556a1a4f195b3cd776164869285f2c
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-19 18:17:14 -0700

    Added downtime component

---
+++ src/lib/Downtime.svelte
@@ -0,0 +1,45 @@
+<main>
+	<section class="h-screen">
+		<div class="hero h-full bg-zinc-900">
+			<div class="hero-content text-center flex-col">
+				<div class="max-w-md">
+					<h1
+						class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-fuchsia-500"
+					>
+						Wuzursched
+					</h1>
+
+					<i class="py-6 text-gray-300 font-mono block">What's your schedule?</i>
+					<div class="alert alert-warning block text-white bg-transparent">
+						<h3 class="text-3xl my-3">School has started!</h3>
+						<p>
+							Wuzursched is currently <i>disabled</i>
+						</p>
+						<p class="bottom-0 text-center font-bold text-3xl mx-auto mt-10 rainbow drop-shadow-lg">
+							See you next year!
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<style>
+	.rainbow {
+		background: linear-gradient(
+			to right,
+			#ef5350,
+			#f48fb1,
+			#7e57c2,
+			#2196f3,
+			#26c6da,
+			#43a047,
+			#eeff41,
+			#f9a825,
+			#ff5722
+		);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+</style>
