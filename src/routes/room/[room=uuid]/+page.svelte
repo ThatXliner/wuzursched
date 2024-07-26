fb1fbc4241d8069424c0ed05cbe1582085a7b774
ThatXliner <bryan.hu.2020@gmail.com>
2024-07-25 19:18:16 -0700

    :sparkles: Closes #55

--- src/routes/room/[room=uuid]/+page.svelte
+++ src/routes/room/[room=uuid]/+page.svelte
@@ -328,7 +328,16 @@
 								d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
 							/></svg
 						>
-						<span>No schedules found. Invite people!</span>
+						<span
+							>No schedules found. <button
+								class="link link-primary"
+								on:click={() => {
+									navigator.clipboard.writeText(window.location.href).then(() => {
+										addToast('Room URL copied to clipboard', 'success');
+									});
+								}}>Invite people!</button
+							></span
+						>
 					</div>
 				</div>
 			{/each}
