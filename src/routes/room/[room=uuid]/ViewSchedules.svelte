ffdd935a893b6e6ffa5685825f9847b799c458f6
ThatXliner <bryan.hu.2020@gmail.com>
2024-07-29 11:29:36 -0700

    :sparkles: Implemented search
    Closes #48

--- src/routes/room/[room=uuid]/ViewSchedules.svelte
+++ src/routes/room/[room=uuid]/ViewSchedules.svelte
@@ -5,6 +5,7 @@
 	import type { Schedule, VirtualSchedule, Class } from '$lib/InfoInput';
 	import { copyToClipboard } from '$lib/actions';
 	import type { You } from './ViewSchedules';
+	import Fuse from 'fuse.js';
 
 	export let schedules: Writable<Schedule[]>;
 	export let you: You;
@@ -15,10 +16,28 @@
 	function matches(a: VirtualSchedule, b: VirtualSchedule) {
 		return PERIODS.some((x) => a[x] === b[x]);
 	}
+	let searchQuery = '';
+	$: fuse = new Fuse($schedules, { keys: ['student'] });
+	$: filtered = searchQuery ? fuse.search(searchQuery).map((x) => x.item) : $schedules;
 </script>
 
+<label class="input input-bordered flex items-center gap-2 w-96 mx-auto my-5">
+	<input type="text" class="grow" placeholder="Search for a student" bind:value={searchQuery} />
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 16 16"
+		fill="currentColor"
+		class="h-4 w-4 opacity-70"
+	>
+		<path
+			fill-rule="evenodd"
+			d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+			clip-rule="evenodd"
+		/>
+	</svg>
+</label>
 <div class="flex flex-wrap justify-evenly">
-	{#each $schedules as schedule}
+	{#each filtered as schedule}
 		<!-- You is guaranteed to !== null -->
 		<!-- I'm not sure how to express TypeScript -->
 		<!-- Within Svelte code -->
