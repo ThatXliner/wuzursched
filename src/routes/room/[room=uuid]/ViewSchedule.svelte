981621e75510c4912bf4390ee95ce70a1a17485d
ThatXliner <bryan.hu.2020@gmail.com>
2026-07-14 17:49:47 -0700

    Make current user schedule first and distinct (#67)

--- src/routes/room/[room=uuid]/ViewSchedule.svelte
+++ src/routes/room/[room=uuid]/ViewSchedule.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-	import { isEqual } from 'lodash-es';
 	import ScheduleDisplay from './ScheduleDisplay.svelte';
 	import type { Schedule, Class } from '$lib/InfoInput';
 	import type { ResolvedYou } from './ViewSchedules';
+	import { isCurrentSchedule } from './schedule-order';
 	let {
 		schedule,
 		you,
 		getClass
 	}: { schedule: Schedule; you: ResolvedYou; getClass: (id: string) => Promise<Class> } = $props();
+	let isYou = $derived(isCurrentSchedule(schedule, you));
 </script>
 
 <div
-	class="my-3 collapse h-fit w-fit collapse-plus border border-base-300 bg-base-100 shadow-xl rounded-box"
+	class="my-3 collapse h-fit w-fit collapse-plus border bg-base-100 shadow-xl rounded-box {isYou
+		? 'border-primary bg-primary/10 ring-2 ring-primary/30'
+		: 'border-base-300'}"
+	data-current-user={isYou || undefined}
 >
 	<input type="checkbox" />
-	<div class="collapse-title text-xl font-medium">
-		{schedule.student}'s schedule {isEqual({ name: schedule.student, schedule }, you)
-			? '(you)'
-			: ''}
+	<div class="collapse-title text-xl font-medium" class:text-primary={isYou}>
+		{schedule.student}'s schedule {isYou ? '(you)' : ''}
 		<!-- TODO: Show if in common -->
 	</div>
 	<div class="collapse-content hidden">
